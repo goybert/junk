9c60ea6e81498f3969bb4ba7e4307c8330837ed2
Riabinin <riabinin@amazon.com>
2015-08-18 13:56:02 -0700

    dicitonary problem

--- Problems/Problems/Dictionary.h
+++ Problems/Problems/Dictionary.h
@@ -11,15 +11,133 @@
 
 #include <stdio.h>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <iterator>
+#include <cassert>
+#include <unordered_map>
+
 
 using namespace std;
-class Dictionary
+class PermutationDictionary
 {
+    unordered_set<string> permutations;
+    friend ostream &operator << (ostream &os, const PermutationDictionary &dict);
 public:
-    void addWord (string str);
+    //adds all permutations of a given word to the dictionary
+    void addWord (const string &str)
+    {
+        vector<char> strvec(str.begin(), str.end());
+        sort(strvec.begin(), strvec.end());
+        do
+        {
+            permutations.emplace(string(strvec.begin(), strvec.end()));
+        }while (next_permutation(strvec.begin(), strvec.end()));
+    }
     
-    
+    void addWords(initializer_list<string> list)
+    {
+        for(auto s: list)
+            addWord(s);
+    }
+    bool containsWord (string str)
+    {
+        return permutations.count(str);
+    }
     
 };
+
+ostream &operator << (ostream &os, const PermutationDictionary &dict)
+{
+    for(auto &s : dict.permutations)
+        cout << s << ";";
+    
+    cout << endl;
+    return os;
+}
+
+void TestPermutationDictionary()
+{
+    PermutationDictionary dict;
+    dict.addWords({"Will", "the", "real", "Slim", "Shady", "please", "stand", "up"});
+    cout << dict;
+    
+    assert(dict.containsWord("real") == true);
+    assert(dict.containsWord("earl") == true);
+    assert(dict.containsWord("rale") == true);
+    assert(dict.containsWord("peasle") == true);
+    assert(dict.containsWord("sdkjhf") == false);
+    
+    
+}
+
+class SortedDictionary
+{
+    unordered_multimap<string, string> dictionaries;
+    friend ostream &operator << (ostream &os, const SortedDictionary &dict);
+public:
+    
+    SortedDictionary(initializer_list<string> ilist)
+    {
+        for (auto s : ilist)
+            addWord(s);
+    }
+    
+    void addWord(const string &str)
+    {
+        string copy(str);
+        sort(copy.begin(), copy.end());
+        dictionaries.emplace(copy, str);
+    }
+    
+    vector<string> getAnagrams(const string &str)
+    {
+        string copy = str;
+        sort(copy.begin(), copy.end());
+        auto range = dictionaries.equal_range(copy);
+        vector<string> ret;
+        for (; range.first != range.second;++range.first)
+            ret.push_back(range.first->second);
+        return ret;
+    }
+    
+
+    
+};
+
+ostream &operator << (ostream &os, const SortedDictionary &dict)
+{
+    for(auto &s : dict.dictionaries)
+        cout << "(" << s.first << "," << s.second << "); ";
+    
+    cout << endl;
+    return os;
+}
+
+template <class T>
+ostream &operator << (ostream &os, const vector<T> v)
+{
+    cout << "(";
+    for(auto &s : v)
+        cout << s << ", " ;
+    
+    cout << ")" << endl;
+    return os;
+}
+
+void TestSortedDictionary()
+{
+    SortedDictionary dict ({"hi", "hello", "elloh", "lleho"});
+
+    cout << dict;
+    
+    cout << dict.getAnagrams("ih");
+    
+    cout << dict.getAnagrams("hello");
+    
+    
+    
+}
 
 #endif /* defined(__Problems__Dictionary__) */
--- Problems/Problems/main.cpp
+++ Problems/Problems/main.cpp
@@ -7,9 +7,13 @@
 //
 
 #include <iostream>
+#include "Dictionary.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    TestSortedDictionary();
+    
+    int i;
+    cin >> i;
     return 0;
 }
